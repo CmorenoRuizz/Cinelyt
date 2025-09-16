e5f1feb4cca05c0084f6b3a8daabbf14b45a7b02
Carlos <camoru@gmail.com>
2025-09-16 10:16:54 +0200

    setting up spring boot backend

---
+++ cinelyt-backend/src/main/java/com/cinelyt/backend/CinelytBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cinelyt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinelytBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinelytBackendApplication.class, args);
+	}
+
+}
---
+++ cinelyt-backend/src/test/java/com/cinelyt/backend/CinelytBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinelyt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinelytBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
