01151de4286baf2fe3d400859d3805ab862f402f
Carlos <camoru@gmail.com>
2025-09-16 11:08:08 +0200

    setting up tailwindcss

---
+++ cinelyt-frontend/src/testtailwind.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        ¡Tailwind funciona!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
